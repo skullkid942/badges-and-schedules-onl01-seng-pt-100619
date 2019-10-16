bc6c9dbd5785f3a877baa8677c40b4f8ad76bf3b
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-16 12:59:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   end 
 end
 
-def batch_badge_maker(name)
+def batch_badge_creator(attendees)
   conference_badges = []
   attendees.each do |attendee|
     conference_badges << name
